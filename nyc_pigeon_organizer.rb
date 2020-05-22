1b17206cf1cf63a67be1449887373f35ffc7d845
Cesar Lara <cesar.lara@yale.edu>
2020-05-22 07:18:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ names.each do |name|
   if !final_array[name][key]
     !final_array[name][key] = []
   end
-  final_array[name][key].push(inner_key)
+  final_array[name][key].push(inner_key.to_s)
 end
 
 end
